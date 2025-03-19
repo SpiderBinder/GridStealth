356eb1e19017f60bdddd84f3f70b1d6993f6b8d3
SpiderBinder <epiceliminator8@gmail.com>
2025-03-19 13:18:48 +0000

    Add minor tweaks in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,43 @@ int main()
     std::cout << "Hello World!" << std::endl;
 
     sf::RenderWindow window(sf::VideoMode(1080, 720), "Hello Window!");
+    window.setFramerateLimit(60);
+
+    sf::Clock clock;
+
+    //Game game(window);
+
+    /*if (!game.init())
+    {
+        return 1;
+    }*/
 
     while (window.isOpen())
     {
+        sf::Event event;
+
+        //calculate delta time
+        sf::Time time = clock.restart();
+        float dt = time.asSeconds();
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+            
+            if (event.type == sf::Event::KeyPressed ||
+                event.type == sf::Event::KeyReleased)
+            {
+
+            }
+        }
+
+        window.clear(sf::Color::Black);
+
         
+        window.display();
     }    
 
     return 0;
