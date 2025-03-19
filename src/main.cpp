e1ea94eea553a34a9073035af2a8d8dd764822fa
SpiderBinder <epiceliminator8@gmail.com>
2025-03-19 11:07:51 +0000

    add CMakeLists

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+
+    sf::RenderWindow window(sf::VideoMode(1080, 720), "Hello Window!");
+
+    while (window.isOpen())
+    {
+        
+    }    
+
+    return 0;
+}
